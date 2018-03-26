c5c232a41628b13a0e76caa1ec92572617c2153b
Florian <fw@studer-raimann.ch>
2018-03-26 14:50:36 +0200

    Updated responsible

--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = "xhfp";
 $version = "0.0.1";
 $ilias_min_version = "5.2.0";
 $ilias_max_version = "5.2.999";
-$responsible = "Florian Wyss";
-$responsible_mail = "fw@studer-raimann.ch";
+$responsible = "studer + raimann ag";
+$responsible_mail = "support@studer-raimann.ch";
