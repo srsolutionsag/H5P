eec279091debcaf9cd59315446cdd374a2469378
Florian <fw@studer-raimann.ch>
2018-07-02 11:25:17 +0200

    Some object list commands

--- classes/class.ilObjH5PListGUI.php
+++ classes/class.ilObjH5PListGUI.php
@@ -18,14 +18,30 @@ class ilObjH5PListGUI extends ilObjectPluginListGUI {
 	 * @return array
 	 */
 	public function initCommands() {
-		$this->timings_enabled = false;
-		$this->subscribe_enabled = false;
-		$this->payment_enabled = false;
-		$this->link_enabled = false;
-		$this->info_screen_enabled = true;
+		$this->commands_enabled = true;
+		$this->copy_enabled = true;
+		$this->cut_enabled = true;
 		$this->delete_enabled = true;
-		$this->cut_enabled = false;
-		$this->copy_enabled = false;
+		$this->description_enabled = true;
+		$this->notice_properties_enabled = true;
+		$this->properties_enabled = true;
+
+		$this->comments_enabled = false;
+		$this->comments_settings_enabled = false;
+		$this->expand_enabled = false;
+		$this->info_screen_enabled = false;
+		$this->link_enabled = false;
+		$this->notes_enabled = false;
+		$this->payment_enabled = false;
+		$this->preconditions_enabled = false;
+		$this->rating_enabled = false;
+		$this->rating_categories_enabled = false;
+		$this->repository_transfer_enabled = false;
+		$this->search_fragment_enabled = false;
+		$this->static_link_enabled = false;
+		$this->subscribe_enabled = false;
+		$this->tags_enabled = false;
+		$this->timings_enabled = false;
 
 		$commands = [
 			[
