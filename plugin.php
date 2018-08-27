f610be3005f0dd86530f2521919dd22ed25f3244
Florian <fw@studer-raimann.ch>
2018-08-27 14:55:42 +0200

    Correct $responsible_mail

--- plugin.php
+++ plugin.php
@@ -4,4 +4,4 @@ $version = "1.0.1";
 $ilias_min_version = "5.2.0";
 $ilias_max_version = "5.3.999";
 $responsible = "studer + raimann ag";
-$responsible_mail = "support@studer-raimann.ch";
+$responsible_mail = "support-custom1@studer-raimann.ch";
