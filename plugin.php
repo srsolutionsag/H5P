972e09f589662f61cbb6f71aea6ad2072b366579
Florian <fw@studer-raimann.ch>
2018-09-13 07:23:57 +0200

    Fix team name

--- plugin.php
+++ plugin.php
@@ -3,5 +3,5 @@ $id = "xhfp";
 $version = "1.1.0";
 $ilias_min_version = "5.2.0";
 $ilias_max_version = "5.3.999";
-$responsible = "studer + raimann ag";
+$responsible = "studer + raimann ag - Team Custom 1";
 $responsible_mail = "support-custom1@studer-raimann.ch";
