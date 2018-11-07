3e770d6a9fee0012a291903a41147c81202005d7
Florian <fw@studer-raimann.ch>
2018-11-07 08:36:17 +0100

    Supports latest libraries

--- classes/uninstall/class.H5PRemoveDataConfirm.php
+++ classes/uninstall/class.H5PRemoveDataConfirm.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . "/../../vendor/autoload.php";
 
-use srag\Plugins\H5P\Option\Option;
 use srag\Plugins\H5P\Utils\H5PTrait;
 use srag\RemovePluginDataConfirm\AbstractRemovePluginDataConfirm;
 
@@ -17,29 +16,4 @@ class H5PRemoveDataConfirm extends AbstractRemovePluginDataConfirm {
 
 	use H5PTrait;
 	const PLUGIN_CLASS_NAME = ilH5PPlugin::class;
-
-
-	/**
-	 * @inheritdoc
-	 */
-	public function getUninstallRemovesData()/*: ?bool*/ {
-		return Option::getUninstallRemovesData();
-	}
-
-
-	/**
-	 * @inheritdoc
-	 */
-	public function setUninstallRemovesData(/*bool*/
-		$uninstall_removes_data)/*: void*/ {
-		Option::setUninstallRemovesData($uninstall_removes_data);
-	}
-
-
-	/**
-	 * @inheritdoc
-	 */
-	public function removeUninstallRemovesData()/*: void*/ {
-		Option::removeUninstallRemovesData();
-	}
 }
--- src/Option/Option.php
+++ src/Option/Option.php
@@ -2,7 +2,6 @@
 
 namespace srag\Plugins\H5P\Option;
 
-use H5PRemoveDataConfirm;
 use ilH5PPlugin;
 use srag\ActiveRecordConfig\ActiveRecordConfig;
 use srag\Plugins\H5P\Utils\H5PTrait;
@@ -19,6 +18,12 @@ class Option extends ActiveRecordConfig {
 	use H5PTrait;
 	const TABLE_NAME = "rep_robj_xhfp_opt_n";
 	const PLUGIN_CLASS_NAME = ilH5PPlugin::class;
+	/**
+	 * @var array
+	 */
+	protected static $fields = [
+
+	];
 
 
 	/**
@@ -38,29 +43,5 @@ class Option extends ActiveRecordConfig {
 	 */
 	public static function setOption($name, $value) {
 		self::setJsonValue($name, $value);
-	}
-
-
-	/**
-	 * @return bool|null
-	 */
-	public static function getUninstallRemovesData()/*: ?bool*/ {
-		return self::getXValue(H5PRemoveDataConfirm::KEY_UNINSTALL_REMOVES_DATA, H5PRemoveDataConfirm::DEFAULT_UNINSTALL_REMOVES_DATA);
-	}
-
-
-	/**
-	 * @param bool $uninstall_removes_data
-	 */
-	public static function setUninstallRemovesData(bool $uninstall_removes_data)/*: void*/ {
-		self::setBooleanValue(H5PRemoveDataConfirm::KEY_UNINSTALL_REMOVES_DATA, $uninstall_removes_data);
-	}
-
-
-	/**
-	 *
-	 */
-	public static function removeUninstallRemovesData()/*: void*/ {
-		self::removeName(H5PRemoveDataConfirm::KEY_UNINSTALL_REMOVES_DATA);
 	}
 }
