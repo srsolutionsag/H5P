f314b65e7a196b2ecd49f1f30ae436f93474f0aa
Florian Wyss <fw@studer-raimann.ch>
2019-03-22 13:59:48 +0100

    Use dropdown ui

--- src/Results/ResultsTableGUI.php
+++ src/Results/ResultsTableGUI.php
@@ -3,7 +3,6 @@
 namespace srag\Plugins\H5P\Results;
 
 use Exception;
-use ilAdvancedSelectionListGUI;
 use ilH5PPlugin;
 use ilObjH5PAccess;
 use ilObjH5PGUI;
@@ -112,10 +111,10 @@ class ResultsTableGUI extends TableGUI {
 
 				$h5p_result = Result::getResultByUserContent($user_id, $h5p_content->getContentId());
 
-				if ($h5p_result !== NULL) {
+				if ($h5p_result !== null) {
 					$this->results[$user_id][$content_key] = ($h5p_result->getScore() . "/" . $h5p_result->getMaxScore());
 				} else {
-					$this->results[$user_id][$content_key] = NULL;
+					$this->results[$user_id][$content_key] = null;
 				}
 			}
 		}
@@ -159,15 +158,15 @@ class ResultsTableGUI extends TableGUI {
 			$user = new ilObjUser($row["user_id"]);
 		} catch (Exception $ex) {
 			// User not exists anymore
-			$user = NULL;
+			$user = null;
 		}
-		$this->tpl->setVariable("USER", $user !== NULL ? $user->getFullname() : "");
+		$this->tpl->setVariable("USER", $user !== null ? $user->getFullname() : "");
 
 		$this->tpl->setCurrentBlock("contentBlock");
 		foreach ($this->contents as $h5p_content) {
 			$content_key = "content_" . $h5p_content->getContentId();
 
-			if ($row[$content_key] !== NULL) {
+			if ($row[$content_key] !== null) {
 				$this->tpl->setVariable("POINTS", $row[$content_key]);
 			} else {
 				$this->tpl->setVariable("POINTS", self::plugin()->translate("no_result"));
@@ -175,18 +174,18 @@ class ResultsTableGUI extends TableGUI {
 			$this->tpl->parseCurrentBlock();
 		}
 
-		$actions = new ilAdvancedSelectionListGUI();
-		$actions->setListTitle(self::plugin()->translate("actions"));
+		$actions = [];
 
 		if (ilObjH5PAccess::hasWriteAccess()) {
-			$actions->addItem(self::plugin()->translate("delete"), "", self::dic()->ctrl()
+			$actions[] = self::dic()->ui()->factory()->button()->shy(self::plugin()->translate("delete"), self::dic()->ctrl()
 				->getLinkTarget($this->parent_obj, ilObjH5PGUI::CMD_DELETE_RESULTS_CONFIRM));
 		}
 
 		$this->tpl->setVariable("FINISHED", self::plugin()->translate($row["finished"] ? "yes" : "no"));
 
-		$this->tpl->setVariable("ACTIONS", self::output()->getHTML($actions));
+		$this->tpl->setVariable("ACTIONS", self::output()->getHTML(self::dic()->ui()->factory()->dropdown()->standard($actions)
+			->withLabel($this->txt("actions"))));
 
-		self::dic()->ctrl()->setParameter($this->parent_obj, "xhfp_user", NULL);
+		self::dic()->ctrl()->setParameter($this->parent_obj, "xhfp_user", null);
 	}
 }
