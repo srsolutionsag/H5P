6a9b06e873e4353bebbcf1212a13ac7b4a574d37
Florian Wyss <fw@studer-raimann.ch>
2019-11-13 10:21:58 +0100

    Update H5P library to 1.24

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "xhfp";
-$version = "2.4.1";
+$version = "2.4.2";
 $ilias_min_version = "5.3.0";
 $ilias_max_version = "5.4.999";
 $responsible = "studer + raimann ag - Team Custom 1";
