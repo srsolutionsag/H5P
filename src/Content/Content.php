917a43a465e34bef624f0dd89c32bf3b06864e58
Florian Wyss <fw@studer-raimann.ch>
2020-02-12 11:51:16 +0100

    Fix

--- src/Content/Content.php
+++ src/Content/Content.php
@@ -211,7 +211,7 @@ class Content extends ActiveRecord
      * @con_length       8
      * @con_is_notnull   true
      */
-    protected $sort;
+    protected $sort = 0;
     /**
      * @var string[]
      *
