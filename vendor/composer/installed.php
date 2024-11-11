e9ebc4d5064ae51bc49f755d3cc72de0a15c9378
Thibeau Fuhrer <thibeau@sr.solutions>
2024-11-11 16:19:31 +0100

    [FIX] #PLH5P-246: update h5p/h5p-editor composer package.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'srag/h5p',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'ced93153f1aee2093a12d8e776ffdefe0bbcd2c6',
+        'pretty_version' => 'dev-release_9',
+        'version' => 'dev-release_9',
+        'reference' => '641e345361a0aaedd3ec4d0d6fab2507e86a4609',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'h5p/h5p-editor' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '8cd9c7fb9a3668fa553b0e093078e87a1fbcdc6d',
+            'reference' => 'cd282161e5ee1006332fdfda909399a35e935244',
             'type' => 'library',
             'install_path' => __DIR__ . '/../h5p/h5p-editor',
             'aliases' => array(
@@ -31,9 +31,9 @@
             'dev_requirement' => false,
         ),
         'srag/h5p' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'ced93153f1aee2093a12d8e776ffdefe0bbcd2c6',
+            'pretty_version' => 'dev-release_9',
+            'version' => 'dev-release_9',
+            'reference' => '641e345361a0aaedd3ec4d0d6fab2507e86a4609',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
